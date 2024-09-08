bb60bea86335b554b5a8f7415f95eff82e7e6275
thanchayawik <thanchayawik@gmail.com>
2024-09-08 20:36:23 +0700

    call sum message

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  await app.listen(3001);
+
+  const microApp = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: '127.0.0.1',
+        port: 8877,
+      },
+    },
+  );
+  await microApp.listen();
 }
 bootstrap();
--- src/math/math.controller.ts
+++ src/math/math.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Logger, Post } from '@nestjs/common';
 import { MathService } from './math.service';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller('math')
 export class MathController {
@@ -9,6 +10,12 @@ export class MathController {
   @Post('sum')
   sum(@Body('numbers') numbers: number[]): number {
     this.logger.log('sum ' + numbers);
+    return this.mathService.accumulate(numbers);
+  }
+
+  @MessagePattern({ cmd: 'sum' })
+  sum2(numbers: number[]): number {
+    this.logger.log('sum2' + numbers);
     return this.mathService.accumulate(numbers);
   }
 }
